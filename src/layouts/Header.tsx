5d62e5d5e35af7400871938f157eff2a5bee40d6
Alexandre-st <alexandre.saintprix@gmail.com>
2025-03-27 23:10:49 +0100

    Add variables

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,4 +1,5 @@
 export const Header = () => {
+    // https://tanstack.com/router/v1/docs/framework/react/examples/start-supabase-basic?path=examples/react/start-supabase-basic/src/routes/signup.tsx
     return (
         <header className="header">
             <div>Kanban</div>
